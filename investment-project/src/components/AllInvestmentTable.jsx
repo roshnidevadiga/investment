d5c253ec45ac23816eb5dfa4bde1eba7a3979f16
roshnidevadiga <roshni.devadiga@gmail.com>
2022-03-12 14:38:33 +0530

    Used mui data grid for investment table

--- investment-project/src/components/AllInvestmentTable.jsx
+++ investment-project/src/components/AllInvestmentTable.jsx
@@ -1,33 +1,47 @@
-import React from "react";
+import * as React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { formatRupees } from "../utils/numberFormatter";
+
+//define columns
+const columns = [
+  { field: "investment_name", headerName: "Investment name", width: 300 },
+  {
+    field: "investment_amount",
+    headerName: "Investment amount",
+    width: 200,
+    valueFormatter: (params) => {
+      return formatRupees(params.value);
+    },
+  },
+  {
+    field: "investment_date",
+    headerName: "Date",
+  },
+  {
+    field: "investment_type",
+    headerName: "Investment type",
+    description: "This column has a value getter and is not sortable.",
+  },
+];
 
 const AllInvestmentTable = () => {
-    //Read data from local storage
-    const investments = JSON.parse(localStorage.getItem("investments"));
-    return (
-        <>
-        <h1>Welcome to my investment Table</h1>
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Amount</th>
-                    <th>Date</th>
-                    <th>Type</th>
-                </tr>
-            </thead>
-            <tbody>
-                {investments.map((investment) => (
-                    <tr key={investment.id}>
-                        <td>{investment.investment_name}</td>
-                        <td>{investment.investment_amount}</td>
-                        <td>{investment.investment_date}</td>
-                        <td>{investment.investment_type}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-        </>
-    );
-    }
+  //Read data from local storage
+  const investments = JSON.parse(localStorage.getItem("investments"));
+  return (
+    <>
+      <h1>Welcome to my investment Table</h1>
+
+      <div style={{ height: 400, width: "80%", margin: "0 auto" }}>
+        <DataGrid
+          rows={investments}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+        />
+      </div>
+    </>
+  );
+};
 
 export default AllInvestmentTable;
